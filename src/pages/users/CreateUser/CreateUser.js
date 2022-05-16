31a020b3b64084519aac543f367274ed25bae7d8
Heisenberg200 <65282454+Heisenberg200@users.noreply.github.com>
2022-05-16 15:11:24 +0300

    create user function added

---
+++ src/pages/users/CreateUser/CreateUser.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./CreateUser.css"
+
+function CreateUser() {
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [err, setError] = useState("");
+    const browse = useNavigate();
+
+    const handleSubmit = (e) => {
+        if (name === '' || surname === '')
+            setError('All the fields must be completed!')
+        else {
+            e.preventDefault();
+            const postUserData = { data: { name, surname } }
+            axios.post('https://pure-caverns-82881.herokuapp.com/api/v54/users', postUserData, {
+                headers: {
+                    "X-Access-Token": '2ef40268b1274769d2657475211ce9742ceaed9d1a08d8049575fa9f18f8b572',
+
+                }
+            }).then((response) => {
+                postUserData.data.id = response.data.id
+                localStorage.setItem("user-data", JSON.stringify(postUserData["data"]))
+                browse("/quizzes")
+            })
+
+        }
+    }
+    return ( <
+        div className = "createUserform" >
+
+        <
+        Link to = "/users"
+        className = "create_user" >
+        <
+        form onSubmit = { handleSubmit } >
+        <
+        h1 className = "header_1_text" > New User < /h1> <
+        label htmlFor = "name"
+        className = "addName" > Name < /label> <
+        input type = "text"
+        className = "addNameForm"
+        value = { name }
+        onChange = {
+            (e) =>
+            setName(e.target.value)
+        } >
+
+        <
+        /input> <
+        label htmlFor = "surname"
+        className = "addSurname" > Surname < /label>
+
+        <
+        input type = "text"
+        className = "addSurnameForm"
+        value = { surname }
+        onChange = {
+            (e) =>
+            setSurname(e.target.value)
+        } >
+
+        <
+        /input> <
+        button type = "submit"
+        className = "btn create_user" > Add User < /button> <
+        /form>
+
+        <
+        /Link> <
+        /div>
+
+    );
+
+
+}
+export default CreateUser;
